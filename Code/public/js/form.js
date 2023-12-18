ebdfc52be456d79e88c5e6a20a0bd5bfa85e01dc
vazqu <vazquezs123@gmail.com>
2023-12-17 19:33:31 -0500

    got attendeelist to populate in mongo after checked in events GUI

--- Code/public/js/form.js
+++ Code/public/js/form.js
@@ -1,6 +1,6 @@
 (function ($) {
   // Let's start writing AJAX calls!
-  let elements = document.getElementsByClassName("post");
+  let postElements = document.getElementsByClassName("post");
   let postUpvotes = $('#post-upvotes');
   postUpvotes.hide();
 
@@ -19,10 +19,10 @@
 
   //Let's get references to our form elements and the div where the todo's will go
   let postsList = $('#post-list');
-  for(elementIndex in elements){
-    let element = elements[elementIndex];
+  for(elementIndex in postElements){
+    let element = postElements[elementIndex];
     // get post upvotees for element
-    bindEventsToCheckbox(elements[elementIndex]);
+    bindEventsToPostsCheckbox(element);
   }
 
   // for(elementIndex in elements){
@@ -55,7 +55,7 @@
     catch(exception) {}
   }
 
-  function bindEventsToCheckbox(div) {
+  function bindEventsToPostsCheckbox(div) {
     if(div.id){
       let post = $(`input[name=upvoteInput${div.id}]`);
       post.id = div.id;
@@ -109,57 +109,12 @@
     // We can store references to our elements; it's better to
     // store them once rather than re-query the DOM traversal each time
     // that the event runs.
-    const firstNumberElement = document.getElementById('number1');
-    const secondNumberElement = document.getElementById('number2');
-    const operationElement = document.getElementById('operation');
 
-    const errorContainer = document.getElementById('error-container');
-    const errorTextElement =
-      errorContainer.getElementsByClassName('text-goes-here')[0];
-
-    const resultContainer = document.getElementById('result-container');
-    const resultTextElement =
-      resultContainer.getElementsByClassName('text-goes-here')[0];
-
-    // We can take advantage of functional scoping; our event listener has access to its outer functional scope
-    // This means that these variables are accessible in our callback
-    staticForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-
-      try {
-        // hide containers by default
-        errorContainer.classList.add('hidden');
-        resultContainer.classList.add('hidden');
-
-        // Values come from inputs as strings, no matter what :(
-        const firstNumberValue = firstNumberElement.value;
-        const secondNumberValue = secondNumberElement.value;
-        const operationValue = operationElement.value;
-        console.log(typeof firstNumberValue);
-        const parsedFirstNumberValue = parseInt(firstNumberValue);
-        console.log(typeof parsedFirstNumberValue);
-        const parsedSecondNumberValue = parseInt(secondNumberValue);
-        const operation = operationStringToFunction(operationValue);
-
-        const result = operation(
-          parsedFirstNumberValue,
-          parsedSecondNumberValue
-        );
-
-        resultTextElement.textContent = 'The result is ' + result;
-        console.log(`${result}`);
-        resultContainer.classList.remove('hidden');
-      } catch (e) {
-        const message = typeof e === 'string' ? e : e.message;
-        errorTextElement.textContent = e;
-        errorContainer.classList.remove('hidden');
-      }
-    });
   }
 
 
   // event checkbox
-  let elements = document.getElementsByClassName("events");
+  let elements = document.getElementsByClassName("event");
   let attendees = $('#attendEvent');
   attendees.hide()
 
@@ -184,23 +139,6 @@
   //   }
   // }
 
-  const validateField = (field) =>{
-    try{
-      if(Array.isArray(field)){
-        // check for present fields
-        if(field.length && field.length > 0)
-          return field;
-        else
-          return "N/A";
-      }
-      if(field != undefined && field != "")
-        return field;
-      else
-        return "N/A";
-    }
-    catch(exception) {}
-  }
-
   function bindEventsToCheckbox(div) {
     if (div.id) {
       let event = $(`input[name=attendEvent${div.id}]`);
@@ -215,7 +153,7 @@
             data: `checked`
           };
           $.ajax(requestConfig).then(function (responseMessage) {
-
+            let temp = 0;
           });
           requestConfig = {
             method: 'GET',
@@ -223,7 +161,7 @@
             data: `checked`
           };
           $.ajax(requestConfig).then(function (responseMessage) {
-
+            let temp = 0;
           });
           
         } else {
@@ -235,7 +173,7 @@
             data: `notChecked`
           };
           $.ajax(requestConfig).then(function (responseMessage) {
-
+            let temp = 0;
           });
           requestConfig = {
             method: 'GET',
@@ -243,7 +181,7 @@
             data: `notChecked`
           };
           $.ajax(requestConfig).then(function (responseMessage) {
-
+            let temp = 0;
           });
         }
       });
